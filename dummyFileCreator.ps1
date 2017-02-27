5a4edb5107e0609806290a73b7a6a28467f410a7
Alexandre <alexandre.krispin@vinx.co.jp>
2017-02-27 16:35:30 +0900

    Add powershell version

---
+++ dummyFileCreator.ps1
@@ -0,0 +1,31 @@
+param (
+[Parameter(Mandatory=$true)][string]$from,
+[Parameter(Mandatory=$true)][string]$to,
+[Parameter(Mandatory=$true)][string]$size
+)
+
+$sw = [Diagnostics.Stopwatch]::StartNew()
+
+Write-Host $from
+Write-Host $to
+Write-Host $size
+
+$filePath = ((Get-Item -Path $from).FullName)
+Write-Host $filePath
+
+DO
+{
+
+    $temp = [IO.file]::ReadAllText($filePath)
+    DO
+    {
+    $temp += $temp
+    } While ($temp.length -lt 10000)
+
+$temp | Out-File -append $to
+Write-Host -NoNewLine ((Get-Item $to).length/1MB) 'MB' `r
+
+} While ((Get-Item $to).length -lt $size)
+
+$sw.Stop()
+Write-Host $sw.Elapsed
