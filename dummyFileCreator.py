660c0208af0e7bec529f51028eef43314b1e21da
Alexandre <alexandre.krispin@vinx.co.jp>
2017-02-28 13:29:27 +0900

    Improve overall experience

--- dummyFileCreator.py
+++ dummyFileCreator.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import click
+from colorama import Style, Fore, Back, init
 import csv
 import datetime as dt
 import os
+import platform
+from pyfiglet import Figlet
 import re
-from sys import getsizeof
+from sys import getsizeof, exit
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -16,11 +19,16 @@ def exists(file):
 
 def not_exists(file):
     if os.path.isfile(os.path.join(DIR_PATH, file)):
-        raise IOError('ファイルは既に存在します')
+        answer = click.prompt(Fore.RED + '''「{}」既に存在します。そのファイルにデータを追加しますか。\n
+              「y」や「n」で返答してください。'''.format(file))
+        if str(answer) == 'y':
+            return True
+        else:
+            print(Fore.GREEN + 'Bye' + Style.RESET_ALL)
+            exit(0)
     return True
 
 def convert_to_bytes(size):
-    print(size)
     size = re.split('(\d+)', size)
     if 'KB' in size:
         return get_size_in_bytes(size[1], '000')
@@ -35,13 +43,13 @@ def get_size_in_bytes(size, zeros):
     return int(str(size) + str(zeros))
 
 def confirmation(source, target, size, encoding, delimiter):
-    print('**************************')
+    print(Fore.GREEN + '**************************')
     print('元のファイル：{}'.format(source))
     print('宛先ファイル：{}'.format(target))
     print('サイズ：{} bytes'.format(size))
     print('文字コード：{}'.format(encoding))
     print('区切り：{}'.format(delimiter))
-    print('**************************')
+    print('**************************' + Fore.RESET)
 
 def reader(source, delimiter, encoding):
     with open(source, 'r', encoding=encoding) as csvfile:
@@ -60,6 +68,16 @@ def writer(target, delimiter, rows):
             csv_writer.writerows(row)
     return True
 
+def welcome():
+    if 'Windows' in platform.platform():
+        os.system('cls')
+    else:
+        os.system('clear')
+
+    fig = Figlet(font='slant')
+    print(Fore.CYAN + fig.renderText('WELCOME'))
+
+
 @click.command()
 @click.option('--source', help='手元のファイル')
 @click.option('--target', help='宛先ファイル')
@@ -72,10 +90,20 @@ def main(source, target, size, delimiter, encoding):
     '''
 
     #
+    # Change background
+    #
+
+    init()
+    print(Back.WHITE)
+
+    welcome()
+
+    #
     # Check source file
     #
     if exists(source):
-        print('{} exists, OK'.format(source))
+        pass
+        # print('{} exists, OK'.format(source))
     else:
         raise IOError('{} does not exist.'.format(source))
 
@@ -83,7 +111,7 @@ def main(source, target, size, delimiter, encoding):
     # Check target file
     #
     if not_exists(target):
-        print('{}を作成します。'.format(target))
+        print('[+] 「{}」にデータを書き込みます。'.format(target))
 
     #
     # Check and convert size
@@ -131,15 +159,16 @@ def main(source, target, size, delimiter, encoding):
             size_in_mb = os.path.getsize(target) / 1000000
 
         size_in_mb = os.path.getsize(target) / 1000000
-        print('宛先ファイルのサイズ: {}MB and container: {}\r'.format(size_in_mb, getsizeof(container)), end='')
+        print(Fore.MAGENTA + '[+] 宛先ファイルのサイズ: {} MB\r'.format(size_in_mb, getsizeof(container)), end='')
     print('\n')
 
     #
     # End time
     #
     t2 = dt.datetime.now()
-    print('実行時間 {}'.format(t1))
+    print(Fore.GREEN + '開始時間 {}'.format(t1))
     print('終了時間 {}'.format(t2))
+    print('実行時間 {}'.format(t2 -t1) + Style.RESET_ALL)
 
 
 if __name__ == '__main__':
