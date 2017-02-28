bc832e645c529eaf450382d6c301b91cb08ca286
Alexandre <alexandre.krispin@vinx.co.jp>
2017-02-28 09:42:22 +0900

    Add NoNewLine to script for powershell - requires Powershell 5.0

--- dummyFileCreator.ps1
+++ dummyFileCreator.ps1
@@ -22,7 +22,7 @@ DO
     $temp += $temp
     } While ($temp.length -lt 10000)
 
-$temp | Out-File -append $to
+$temp | Out-File -append $to -NoNewLine
 Write-Host -NoNewLine ((Get-Item $to).length/1MB) 'MB' `r
 
 } While ((Get-Item $to).length -lt $size)
