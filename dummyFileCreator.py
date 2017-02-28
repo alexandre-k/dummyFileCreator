27452605b51f5f2766236153ff71f426352ca95c
Alexandre <alexandre.krispin@vinx.co.jp>
2017-02-28 10:08:12 +0900

    Modify parameter for delimiter so that it works in when executed from Powershell, since \t seems to be escaped

--- dummyFileCreator.py
+++ dummyFileCreator.py
@@ -34,11 +34,13 @@ def convert_to_bytes(size):
 def get_size_in_bytes(size, zeros):
     return int(str(size) + str(zeros))
 
-def confirmation(source, target, size):
+def confirmation(source, target, size, encoding, delimiter):
     print('**************************')
     print('元のファイル：{}'.format(source))
     print('宛先ファイル：{}'.format(target))
     print('サイズ：{} bytes'.format(size))
+    print('文字コード：{}'.format(encoding))
+    print('区切り：{}'.format(delimiter))
     print('**************************')
 
 def reader(source, delimiter, encoding):
@@ -62,7 +64,7 @@ def writer(target, delimiter, rows):
 @click.option('--source', help='手元のファイル')
 @click.option('--target', help='宛先ファイル')
 @click.option('--size', help='宛先ファイルのサイズ')
-@click.option('--delimiter', help='コマやタブの区切り：,や\t')
+@click.option('--delimiter', help='コマやタブの区切り：commaやtab')
 @click.option('--encoding', help='文字コードの種類：shift_jisやutf-8')
 def main(source, target, size, delimiter, encoding):
     '''
@@ -95,9 +97,17 @@ def main(source, target, size, delimiter, encoding):
         os.utime(DIR_PATH, None)
 
     #
+    # Delimiter
+    #
+    if delimiter == 'tab':
+        delimiter = '\t'
+    if delimiter == 'comma':
+        delimiter = ','
+
+    #
     # Confirmation
     #
-    confirmation(source, target, size)
+    confirmation(source, target, size, encoding, delimiter)
 
     #
     # Read file
@@ -128,8 +138,8 @@ def main(source, target, size, delimiter, encoding):
     # End time
     #
     t2 = dt.datetime.now()
-    print('t1 {}'.format(t1))
-    print('t2 {}'.format(t2))
+    print('実行時間 {}'.format(t1))
+    print('終了時間 {}'.format(t2))
 
 
 if __name__ == '__main__':
